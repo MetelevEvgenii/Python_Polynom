5d6aac8d1ca089b105d293772f808b87b6b9ee19
metelev.evgenii <metelev.evgenii@yandex.ru>
2018-04-01 22:29:15 +0300

    working minus with 1 and -1

--- polynomTests.py
+++ polynomTests.py
@@ -10,16 +10,14 @@ class Polynom(object):
         iterator = 0
         for i in self.coeffs:
             if i != 0:
-                if i == 1:
-                    if iterator != 0:
+                if iterator != 0:
+                    if i > 0:
                         st += "+"
-                    if iterator == len(self.coeffs)-1:
-                        st += str(i)
-                else:
-                    if iterator != 0:
-                        if i > 0:
-                            st += "+"
+                if i == -1:
+                    st += "-"
+                elif i != 1 or iterator == len(self.coeffs)-1:
                     st += str(i)
+
                 degree = len(self.coeffs) - iterator - 1
                 if degree > 1:
                     st += "x^" + str(degree)
@@ -58,6 +56,11 @@ class TestPolynom(unittest.TestCase):
         list1 = [1, -7, 1, -3, 5, -10]
         pol = Polynom(list1)
         self.assertEqual("x^5-7x^4+x^3-3x^2+5x-10", pol.__str__())
+
+    def testStrOutWithMines2(self):
+        list1 = [-1, -1, -2, -3, -5, -10]
+        pol = Polynom(list1)
+        self.assertEqual("-x^5-x^4-2x^3-3x^2-5x-10", pol.__str__())
 
 pass
 
